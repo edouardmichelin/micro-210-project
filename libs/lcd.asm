0cac4321cca39b0dc7d664d78ea4c3b0bf7fbbe1
unknown <edouard.michelin15@gmail.com>
2023-05-22 00:06:12 +0200

    Initial commit

---
+++ libs/lcd.asm
@@ -0,0 +1,126 @@
+/*
+ * lcd.asm
+ *
+ *  Created: 5/21/2023 4:00:13 PM
+ *   Author: Edouard
+ */ 
+
+.include "libs/printf.asm"
+
+
+.macro	PRINT_ERROR
+		rcall		LCD_clear
+		PRINTF		LCD_putc
+.db		"Error", LF, CR, 0
+		.endmacro
+
+
+.macro	LCD_PRINT
+		rcall		LCD_clear
+		PRINTF		LCD_putc
+		.endmacro
+
+.equ	PRINTABLE_LEN = 16
+.equ	PRINT_SPEED = 200
+; string addr in z, number of cycles in r16
+circular_print:
+		mov			r24,		zl
+		mov			r25,		zh
+		clr			r22								; r22 = cycle counter
+		rcall		strslen							; r23 = len(z)
+circular_print_loop_pre:							; this loop is for printing multiple cycles
+		clr			r17								; r17 = current start position
+circular_print_loop:								; this loop is for printing 1 entire cycle
+		push		r16
+		push		r17
+		push		r22
+		push		r23
+		push		r24
+		push		r25
+		rcall		LCD_clear
+		pop		r24
+		pop		r24
+		pop			r23
+		pop			r22
+		pop			r17
+		pop			r16
+
+		clr			r19								; r19 = current index
+		mov			zl,			r24
+		mov			zh,			r25
+		add			zl,			r17					; increment the address of the string
+		adc			zh,			r19					; r19 = 0 => zh = 0 + C (carry flag)
+circular_print_loop_puts:							; this loop is for 1 screen print
+		lpm			r21,		z					; load content of z into r21
+		cpi			r19,		PRINTABLE_LEN		; check that index of next char to be printed is less than PRINTABLE_LEN
+		brge		circular_print_loop_puts_done	; break from this loop if it is the case
+		mov			a0,			r21					; copy content of r21 into a0
+		tst			r21								; check if r21 = '\0'
+		breq		circular_print_loop_puts_done	; break from this loop if it is the case
+		
+		push		r16
+		push		r17
+		push		r18
+		push		r19
+		push		r20
+		push		r21
+		push		r22
+		push		r23
+		push		r24
+		push		r25
+		rcall		LCD_putc						; print the char
+		pop			r25
+		pop			r24
+		pop			r23
+		pop			r22
+		pop			r21
+		pop			r20
+		pop			r19
+		pop			r18
+		pop			r17
+		pop			r16
+		
+		adiw		z,			1					; increment z pointer
+		inc			r19								; increment the current index in the string
+		rjmp		circular_print_loop_puts		; continue printing characters
+circular_print_loop_puts_done:
+		push		r16
+		push		r17
+		push		r22
+		push		r23
+		push		r24
+		push		r25
+		WAIT_MS		PRINT_SPEED						; wait before "sliding" the text
+		pop			r25
+		pop			r24
+		pop			r23
+		pop			r22
+		pop			r17
+		pop			r16
+
+		inc			r17								; increment the starting position
+		cp			r17,		r23					; check if string has done the entire cycle
+		breq		circular_print_loop_done		; if it is the case, start again from the beginning
+		rjmp		circular_print_loop
+circular_print_loop_done:
+		inc			r22
+		cp			r22,		r16					; check if we have reached the max number of cycles
+		brge		circular_print_ret
+		jmp			circular_print_loop_pre
+circular_print_ret:
+		rcall		LCD_clear
+		ret
+		
+
+
+strslen:											; returns the string length (z) in reg r23
+		clr			r23
+strlen_tst:
+		lpm			r17,		z
+		tst			r17
+		breq		strstrlen_ret
+		inc			r23
+		adiw		z,			1
+		rjmp		strlen_tst
+strstrlen_ret:
+		ret
---
+++ main.asm
@@ -0,0 +1,196 @@
+;
+; project.asm
+;
+; Created: 5/21/2023 3:55:21 PM
+; Author : Edouard
+;
+.include "definitions.asm"
+.include "macros.asm"
+
+.equ	ZERO_FLAG = 1
+.equ	LEDS_STATUS = 0x200
+.equ	LEDS_DEFAULT_VALUE = 0b11111111
+
+.equ	CURR_PLAYER = 0x502
+.equ	CURR_MENU = 0x504
+
+.equ	MENU0addr = 0x550
+.equ	MENU1addr = 0x552
+.equ	MENU2addr = 0x554
+.equ	MENU3addr = 0x556
+
+.equ	NUM_MENUS = 4
+.equ	MAX_PLAYERS = 9
+.equ	MAX_SOLO_ROUNDS = 9
+
+
+.def	menu_reg = r18
+
+
+
+; ============ IVT ============
+.org	0
+		jmp			reset
+
+.org	OVF0addr
+		jmp			handle_timer_ovf
+
+
+		
+
+; ============ DRIVERS ============
+.include "drivers/lcd.asm"
+
+; ============ LIBS ============
+.include "libs/lcd.asm"
+
+		
+
+
+; ============ MACROS ============
+
+; Reset LEDs and LEDs status
+.macro	RESET_LEDS
+		OUTI		PORTB,			0xFF			; Turn LEDs off
+		ldi			r16,			LEDS_DEFAULT_VALUE
+		sts			LEDS_STATUS,	r16
+		.endmacro
+		
+
+.macro	CHECK_MENU ; menu id, branch location
+		cpi			menu_reg,		@0
+		brne		CHECK_MENU_no_match
+		jmp			@1
+CHECK_MENU_no_match:
+		.endmacro
+
+
+.macro	NEXT_MENU
+		lds			menu_reg,		CURR_MENU
+		inc			menu_reg
+		sts			CURR_MENU,		menu_reg
+		.endmacro	
+
+
+
+; ============ INT HANDLERS ============
+
+handle_timer_ovf:
+		push	r16
+		; INCREMENT LEDS COUNTER
+		lds			r16,			LEDS_STATUS
+		lsr			r16
+		sts			LEDS_STATUS,	r16
+		out			PORTB,			r16
+
+		; CHECK IF TIMES IS UP
+		brbs		ZERO_FLAG,		handle_timer_ovf_player_out_of_time
+handle_timer_ovf_ret:
+		pop			r16
+		reti
+handle_timer_ovf_player_out_of_time:
+		RESET_LEDS		
+		
+		lds			r16,			CURR_PLAYER
+		inc			r16
+		sts			CURR_PLAYER,	r16
+
+		rjmp		handle_timer_ovf_ret
+
+
+
+
+; ============ PROGRAM ============
+reset:
+		; INIT STACK
+		LDSP		RAMEND
+
+		; INIT/RESET PROGRAM MEMORY
+		ldi			r16,			0
+		sts			LEDS_STATUS,	r16
+		sts			CURR_PLAYER,	r16
+		sts			CURR_MENU,		r16
+
+		; INIT LEDS
+		OUTI		DDRB,			0xFF			; Make PORTB output
+		RESET_LEDS
+
+		; INIT LCD
+		rcall		LCD_init
+
+
+		; INIT TIMER
+		OUTI		TIMSK,			(1 << TOIE0)	; Timer0 overflow interrupt enable
+		OUTI		ASSR,			(1 << AS0)		; Clock from TOSC1 (external)
+		OUTI		TCCR0,			6				; CK/256 => overflow every 2s
+
+		sei											; Enable interrupts
+
+		rjmp		main
+
+
+welcome_message: .db "Bienvenue ! Pour demarrer, pressez le bouton MUTE de la telecommande.", 0
+how_to_play: .db "Placez la carte STK300 sous une surface, puis appuyez sur la touche MUTE de la telecommande afin de mesurer la distance et lancer la partie.", 0
+number_of_players: .db "Combien de joueurs participeront au jeu ? Entrez une valeur entre 1 et 9 a l'aide de la telecommande.", 0
+number_of_rounds: .db "Lorsque vous etes seul, vous pouvez choisir le nombre de tentatives auxquelles vous aurez le droit. Entrez une valeur entre 1 et 0 a l'aide de la telecommande.", 0
+
+main:
+		rcall		LCD_clear
+
+		lds			menu_reg,		CURR_MENU
+
+		CHECK_MENU	0,				menu0
+		CHECK_MENU	1,				menu1
+		CHECK_MENU	2,				menu2
+		CHECK_MENU	3,				menu3
+
+main_after:
+		; NEXT_MENU
+		; Check if we want to go to a menu that does not exist
+		lds			menu_reg,		CURR_MENU
+		cpi			menu_reg,		NUM_MENUS
+		brge		main_after_menu_overflow
+		WAIT_MS		5000
+		rjmp		main
+main_after_menu_overflow:
+		jmp			inf_loop
+
+menu0:
+		LDIZ		2*welcome_message
+		ldi			r16,			2
+		rcall		circular_print
+		; Simulate start
+		WAIT_MS		2000
+		NEXT_MENU
+		jmp			main_after
+
+
+menu1:
+		LDIZ		2*how_to_play
+		ldi			r16,			1
+		rcall		circular_print
+		; Simulate start
+		WAIT_MS		5000
+		NEXT_MENU
+
+		jmp			main_after
+
+
+
+menu2:
+		PRINTF		LCD_putc
+.db		"Menu 2", 0
+
+		jmp			inf_loop
+		jmp			main_after
+
+
+
+menu3:
+		PRINTF		LCD_putc
+.db		"Menu 3", 0
+		jmp			main_after
+
+
+inf_loop:
+		rjmp		inf_loop
